f95585a8614989c2902545c2c5e9dc187a199148
rghidle <rhi@voltpoweranalytics.com>
2024-09-28 12:25:08 +0200

    .

--- ConsOptimization/TET4565_Assignment-2.py
+++ ConsOptimization/TET4565_Assignment-2.py
@@ -248,7 +248,7 @@ hourly_consumption(client_data[cl_num])
 # Save the data to a new csv file
 client_data[cl_num].to_csv('house9_data.csv', sep=',')
 
-Prices_df = pd.read_csv('Raw_price_data/Texas_prices_June.csv', sep=',')
+Prices_df = pd.read_csv('Raw_price_data/Texas_prices_August.csv', sep=',')
 Prices_df = Prices_df[Prices_df['Zone'] == 'LZ_AEN']
 Prices_df.index = pd.to_datetime(Prices_df['Date'])
 Prices_df.to_csv('Prices_July.csv', sep=',')
